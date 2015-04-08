fadbc76b41755f4cbb114d6ad2ca471498b8302e
Martin N <mr.avgp@googlemail.com>
2015-04-08 09:10:31 +0000

    Integrating S3

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 var fs = require('fs'),
+    S3 = require('streaming-s3'),
     exif = require('exif-parser'),
+    mongo = require('mongojs'),
     restify = require('restify'),
     shortid = require('shortid');
+
+// Init global stuff
+
+var db = mongo(process.env.MONGOLAB_URI, ['teleports']);
+
+// Helpers
+
+function uploadToS3(localFile, uploadName, errorCallback, successCallback) {
+
+  var fStream = fs.createReadStream(localFile);
+  var uploader = new S3(fStream, {accessKeyId: process.env.AWS_ACCESS_KEY, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY},
+    {
+      Bucket: 'teleports',
+      Key: 'portals/' + uploadName,
+      ContentType: 'image/jpeg'
+    }
+  );
+
+  uploader.begin();
+
+  uploader.on('data', function (bytesRead) {
+    console.log(bytesRead, ' bytes read.');
+  });
+
+  uploader.on('part', function (number) {
+    console.log('Part ', number, ' uploaded.');
+  });
+
+  // All parts uploaded, but upload not yet acknowledged.
+  uploader.on('uploaded', function (stats) {
+    console.log('Upload stats: ', stats);
+  });
+
+  uploader.on('finished', function (resp, stats) {
+    console.log('Upload finished: ', resp);
+    successCallback();
+  });
+
+  uploader.on('error', errorCallback);
+}
+
+function saveToDatabase(record, errorCallback, successCallback) {
+  db.teleports.insert(record, function(err) {
+    if(err) errorCallback(err);
+    else successCallback();
+  });
+}
+
+// Handlers
 
 function listSpots(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -38,21 +89,30 @@ function addSpot(req, res, next) {
       // i don't care.
     }
 
-    console.log(metadata.tags.GPSLatitude, metadata.tags.GPSLongitude);
+    var fileOutName = req.files.file.name.replace('.jpg', '') + "_" + shortid.generate(),
+        teleport = { file: fileOutName };
 
-    var fileOutName = req.files.file.name.replace('.jpg', '') + "_" + shortid.generate();
+    if(metadata && metadata.tags) {
+      teleport.lat = metadata.tags.GPSLatitude;
+      teleport.lng = metadata.tags.GPSLongitude;
+    }
 
-    fs.writeFile(fileOutName, imgContent, function(err, data) {
-      if(err) {
-        console.error("Error writing file " + fileOutName);
+    uploadToS3(req.files.file.path, fileOutName + '.jpg', function error(err) {
+      console.error("Error saving to S3 ", err, teleport);
+      res.header('Location', req.params.callback + '?error=true');
+      res.send(302, 'An error happened :(');
+    }, function success() {
+      console.log("S3 Upload successful");
+      saveToDatabase(teleport, function(err) {
+        console.error("Error saving to DB: " + err, teleport);
         res.header('Location', req.params.callback + '?error=true');
         res.send(302, 'An error happened :(');
-        return;
-      }
-
-      res.header('Location', req.params.callback + '?public_id=' + fileOutName);
-      res.send(302, fileOutName);
-      next();
+      }, function() {
+        console.log("DB insert successful");
+        res.header('Location', req.params.callback + '?public_id=' + fileOutName);
+        res.send(302, fileOutName);
+        next();
+      });
     });
   });
 }
